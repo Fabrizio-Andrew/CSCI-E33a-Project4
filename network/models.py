35d8a80959ae0f2b5cb5dfee6246e7d95bdf6b5d
Andrew <andrewdfabrizio@gmail.com>
2020-11-10 22:52:47 -0500

    Designed models

--- network/models.py
+++ network/models.py
@@ -3,4 +3,38 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    """
+    Contains all data pertaining to users and accounts.
+    """
+    username = models.CharField(max_length=64, unique=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=256)
+    followers = models.ManyToManyField(
+        'User',
+        blank=True,
+        related_name='peoplefollowing'
+    )
+
+    def __str__(self):
+        return f"<{self.pk}: {self.username}>"
+
+
+class Post(models.Model):
+    """
+    Contains all data pertaining to user posts.
+    """
+    content = models.CharField(max_length=256)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(
+        'User',
+        related_name='likers'
+    )
+    poster = models.ForeignKey(
+        'User',
+        related_name='poster',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"<{self.pk}: post by {self.poster}>"
+
